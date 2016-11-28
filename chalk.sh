ab9f65299667e4a7926c78a92d003665ace905d1
Leo Bergnéhr <leo@bergnehr.se>
2016-11-28 21:25:15 +0100

    Speak each character before the complete word

--- chalk.sh
+++ chalk.sh
@@ -1,5 +1,18 @@
-while [ 1 ];
-do
-  read -p "" word
-  say $word
+#!/bin/bash
+
+set -e
+
+REPLY=""
+word=""
+IFS=''
+while [[ "$REPLY" != $'\x0a' && "$REPLY" != $'\x20' ]]; do
+  read -r -d'' -s -n1
+  letter=$REPLY
+
+  say $letter
+  word="$word$letter"
 done
+
+if [ -n $word ]; then
+  say $word
+fi
