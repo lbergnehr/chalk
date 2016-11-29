c11bfd0106df49194da422119e0a653a33a12add
Leo Bergnéhr <leo@bergnehr.se>
2016-11-29 19:36:57 +0100

    Say letters when pressed in addition to whole word

--- chalk.sh
+++ chalk.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-REPLY=""
-word=""
-IFS=''
-while [[ "$REPLY" != $'\x0a' && "$REPLY" != $'\x20' ]]; do
-  read -r -d'' -s -n1
-  letter=$REPLY
+while [ 1 ]; do
+  REPLY=""
+  word=""
+  IFS=''
+  while [[ "$REPLY" != $'\x0a' && "$REPLY" != $'\x20' ]]; do
+    read -r -d'' -s -n1
+    letter=$REPLY
 
-  say $letter
-  word="$word$letter"
+    say $letter &
+    word="$word$letter"
+  done
+
+  if [ -n $word ]; then
+    say $word
+  fi
 done
-
-if [ -n $word ]; then
-  say $word
-fi
