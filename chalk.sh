4b370256db08f7cd1424d54df7780b888bdea56f
Leo Bergnéhr <leo@bergnehr.se>
2016-11-29 19:39:17 +0100

    Uppercase `WORD`

--- chalk.sh
+++ chalk.sh
@@ -4,17 +4,17 @@ set -e
 
 while [ 1 ]; do
   REPLY=""
-  word=""
+  WORD=""
   IFS=''
   while [[ "$REPLY" != $'\x0a' && "$REPLY" != $'\x20' ]]; do
     read -r -d'' -s -n1
     letter=$REPLY
 
     say $letter &
-    word="$word$letter"
+    WORD="$WORD$letter"
   done
 
-  if [ -n $word ]; then
-    say $word
+  if [ -n $WORD ]; then
+    say $WORD
   fi
 done
